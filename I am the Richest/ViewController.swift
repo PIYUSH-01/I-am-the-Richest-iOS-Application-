0277792bf64ed86fb08c1740a3d9fcf56347c24e
Piyush Khurana <piyushkhurana165@gmail.com>
2020-10-05 23:31:16 +0530

    Added the other files

---
+++ I am the Richest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am the Richest
+//
+//  Created by PIYUSH KHURANA on 03/10/20.
+//  Copyright © 2020 PIYUSH KHURANA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
